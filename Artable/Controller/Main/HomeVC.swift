6b6c2d4e11e7f22710633765dbfcbc8e7af09610
Chris <chris.sedano.arenas@gmail.com>
2020-07-05 11:51:04 +0100

    UserService
    Create new group called Services and new swift File called UserServices, and inside some variables some func getCurrentuser and logoutUser, inside HomeVC.swift put that function in logOutClicked and viewDidAppear

--- Artable/Controller/Main/HomeVC.swift
+++ Artable/Controller/Main/HomeVC.swift
@@ -63,6 +63,11 @@ class HomeVC: UIViewController {
         
         if let user = Auth.auth().currentUser, !user.isAnonymous {
             loginOutButton.title = "Logout"
+            
+            if UserService.userListener == nil {
+                UserService.getCurrentUser()
+            }
+            
         } else {
             loginOutButton.title = "Login"
         }
@@ -114,6 +119,7 @@ class HomeVC: UIViewController {
         } else {
             do {
                 try Auth.auth().signOut()
+                UserService.logoutUser()
                 Auth.auth().signInAnonymously { (result, error) in
                     if let error = error {
                         Auth.auth().handleFireAuthError(error: error, viewController: self)
